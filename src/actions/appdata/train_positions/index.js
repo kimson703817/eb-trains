adc196269d8828b675398e8cecd2cdb64473636b
Kim Son <kimson703817@gmail.com>
2019-07-01 04:43:05 -0700

    troubleshooting api key in production

--- src/actions/appdata/train_positions/index.js
+++ src/actions/appdata/train_positions/index.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 export const fetchLiveTP = () => async dispatch => {
   const { api } = WMATA;
   const { api_key } = keys.WMATA;
-  console.log(api_key);
+  console.log(keys);
   const req = {
     method: 'get',
     url: `${api}/trainpositions/trainpositions?contentType=json`,
